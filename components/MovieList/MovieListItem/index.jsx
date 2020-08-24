7fc966ab96beb28f04d1955d31d4d551a579569a
lucascamposj <lucascj29@gmail.com>
2020-08-24 19:00:50 -0300

    adds fallback checking

--- components/MovieList/MovieListItem/index.jsx
+++ components/MovieList/MovieListItem/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import {Container, InfoContainer} from './styles';
 
-const MovieListItem = ({movie}) => (
-  <Container>
-    <Link href={'/movies/[movie]'} as={`/movies/${movie.imdbID}`} passHref>
-      <a>
-        <img src={movie.Poster} alt={movie.Title}/>
-      </a>
-    </Link>
-    <InfoContainer>
-      <h1>{movie.Title}</h1>
-      <h2>{movie.Year}</h2>
-    </InfoContainer>
-  </Container>
-);
+const MovieListItem = ({movie}) => {
+  const router = useRouter();
+
+  if(router.isFallback){
+    return <h1>Loading...</h1>
+  }
+
+  return(
+    <Container>
+      <Link href={'/movies/[movie]'} as={`/movies/${movie.imdbID}`} passHref>
+        <a>
+          <img src={movie.Poster} alt={movie.Title}/>
+        </a>
+      </Link>
+      <InfoContainer>
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Year}</h2>
+      </InfoContainer>
+    </Container>
+  )
+};
 
 export default MovieListItem;
